7b5fe5e1538c4892a8c791eaadbf3306cdf84bb6
axelinstrumental <aobscura@instrumental.net>
2025-03-18 18:03:52 -0600

    contenidos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -11,7 +13,7 @@ export default function Home() {
           height={200}
           alt="Logo"
           />
-          <p>WHATSAPP</p>
+          <FaWhatsapp color="#09a32a" size={75} className="mr-2" />
         </div>
         <div className="w-full bg-[url('/uno.jpg')] min-h-screen bg-cover bg-center flex flex-col items-start justify-center">
           <h1 className="px-10 text-8xl text-white font-bold" style={{
@@ -90,10 +92,47 @@ export default function Home() {
           <h1 className="px-10 text-6xl text-slate-900 font-bold p-5 w-full text-center" style={{
             textShadow: '4px 4px 4px rgba(244,126,1,0.5)'
           }}>Features</h1>
+            <div className="grid grid-cols-[1fr_1fr] items-stretch justify-start w-full p-10 gap-3">
+            <div className="bg-gray-100 flex items-center">   
+              <p className="text-gray-700 text-left font-bold text-2xl p-5">Our hurricane screen reduces hurricane and wind velocity up to 95%.</p>
+            </div>
+            <div className="bg-gray-100 flex items-center">   
+              <p className="text-gray-700 text-left font-bold text-2xl p-5">Florida building code approved (FL-1129)</p>
+            </div>
+            <div className="bg-gray-100 flex items-center">   
+              <p className="text-gray-700 text-left font-bold text-2xl p-5">Straps can be securely tightened for increase tautness</p>
+            </div>
+            <div className="bg-gray-100 flex items-center">   
+              <p className="text-gray-700 text-left font-bold text-2xl p-5">Miami Dade (Noa 140207-0)</p>
+            </div>
+            <div className="bg-gray-100 flex items-center">   
+              <p className="text-gray-700 text-left font-bold text-2xl p-5">Filters in light the Room, enabling you to see out</p>
+            </div>
+            <div className="bg-gray-100 flex items-center">   
+              <p className="text-gray-700 text-left font-bold text-2xl p-5">Available in multiple colors, to blend your home’s exterior</p>
+            </div>
+            </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center w-full bg-gray-200">
+          <div className="grid grid-cols-[1fr_1fr_1fr] items-center justify-items-center w-full p-10 gap-3 font-bold">
+            <div className="flex flex-col items-center justify-center">
+              <Image
+                src="/logo.png"
+                width={200}
+                height={200}
+                alt="Logo"
+              />
+              <p className="mt-3">The best decision for your assets!</p>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <p className="flex justify-center items-center mb-3"><FaMapMarkerAlt color="#ff0000" size={25} className="mr-2" /> San José Del Cabo B.C.S</p>
+              <p className="flex justify-center items-center"><FaWhatsapp color="#09a32a" size={25} className="mr-2" />+52 1 624 122 0302</p>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <p>hola@strongprotectionhurricane.com</p>
+            </div>
+          </div>
       </footer>
     </div>
   );
